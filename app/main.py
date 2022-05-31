22052a6827a11e81d612ab25cbaf9b32fd57d404
crblanaru <crblanaru@gmail.com>
2022-05-31 13:51:11 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,4 +7,4 @@ app = FastAPI()
 @app.get("/")
 def hello_world():
     api_key = os.getenv('API_KEY')
-    return {"bunnyshell-neo": "awesome", "api_key": api_key}
+    return {"bunnyshell-neo": "is_awesome", "api_key": api_key}
