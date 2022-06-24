d5c451baea86b494c16f24b5dabf792f57c9818b
Cristian <crblanaru@gmail.com>
2022-06-25 02:29:01 +0300

    corrected spelling

--- app/main.py
+++ app/main.py
@@ -7,4 +7,4 @@ app = FastAPI()
 @app.get("/")
 def hello_world():
     api_key = os.getenv('API_KEY')
-    return {"bunnyshell-neo": "is_amazing", "api_keyz": api_key}
+    return {"bunnyshell-neo": "is_amazing", "api_keys": api_key}
