c56051a493bc8babb494a6f52f2cfc08a81eda35
Cristian <crblanaru@gmail.com>
2022-06-26 00:31:32 +0300

    added code for container replacement

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import os
+import random
 
 app = FastAPI()
 
@@ -7,4 +8,6 @@ app = FastAPI()
 @app.get("/")
 def hello_world():
     api_key = os.getenv('API_KEY')
+    my_rand = random.randint(100,999)
     return {"bunnyshell-neo": "is_amazing", "api_keyz": api_key}
+    #return {"bunnyshell-neo": "is_amazing", "api_keyz": api_key, "random": my_rand}
