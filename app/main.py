5da414bb3bc9a9e5a65fb9dc3ff754d24804cdc6
crblanaru <crblanaru@gmail.com>
2022-07-08 16:15:35 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ app = FastAPI()
 @app.get("/")
 def hello_world():
     api_key = os.getenv('API_KEY')
+    db_endpoint = os.getenv('DBENDPOINT')
     my_rand = random.randint(100,999)
-    return {"bunnyshell-neo": "is_amazing", "api_keyz": api_key}
+    return {"bunnyshell-neo": "is_amazing", "api_keyz": api_key, "db_endpoint": db_endpoint}
     #return {"bunnyshell-neo": "is_amazing", "api_keyz": api_key, "random": my_rand}
