f55c016367162e23f5f8d8ad00dd8533b6772760
crblanaru <crblanaru@gmail.com>
2022-07-15 15:30:23 +0200

    Update main.py
    can we

--- app/main.py
+++ app/main.py
@@ -11,4 +11,4 @@ def hello_world():
     db_endpoint = os.getenv('DBENDPOINT')
     my_rand = random.randint(100,999)
     return {"bunnyshell-neo": "is_amazing", "api_key": api_key, "db_endpoints": db_endpoint}
-    #return {"bunnyshell-neo": "is_amazing", "api_key": api_key, "random": my_rand}
+    #return {"bunnyshell-neo": "is_amazing", "api_keys": api_key, "random": my_rand}
