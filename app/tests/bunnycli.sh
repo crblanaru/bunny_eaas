6d1af113576591afae8fc51c2356c24400ac2858
cristianbl <crblanaru@gmail.com>
2022-07-18 17:09:47 +0200

    tackling slashes

--- app/tests/bunnycli.sh
+++ app/tests/bunnycli.sh
@@ -6,7 +6,7 @@ sleep 10
 # finding environment id from PR
 envid=$(curl -s -X 'GET' 'https://api.environments.bunnyshell.com/api/projects/168/environments?page=1' \
   -H 'accept: application/ld+json' \
-  -H "X-AUTH-TOKEN: $AUTH_TOKEN" | grep -Po "id.*$GIT_BRANCH" | sed -E 's/.*"id":"?([^,"]*)"?.*/\1/')
+  -H "X-AUTH-TOKEN: $AUTH_TOKEN" | grep -Po "id.*$GIT_BRANCH" | sed -E 's#.*"id":"?([^,"]*)"?.*#\1#')
 
 [[ -z "$envid" ]] && { echo "envid is empty" ; exit 1; }
   
@@ -22,6 +22,10 @@ while ! curl -s -X 'GET' \
   -H 'accept: application/ld+json' \
   -H "X-AUTH-TOKEN: $AUTH_TOKEN" | grep "status.*running" 
 do
-echo "wait for deployment to be ready"
-sleep 10
+  echo "wait for deployment to be ready"
+  sleep 10
 done
+
+echo "ready to start testing"
+
+curl "https://api-$unique.bunnyenv.com/" | grep "bunnyshell-neo.*is_amazing"
