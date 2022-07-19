f68f9a2ac00c49749ac7980a44b4bd6399b3ce06
crblanaru <crblanaru@gmail.com>
2022-07-19 16:59:44 +0200

    Update bunnycli.sh

--- app/tests/bunnycli.sh
+++ app/tests/bunnycli.sh
@@ -3,13 +3,13 @@ sleep 10
 
 echo '-- extracting information about the pull request'
 # finding environment id from PR
-envid=$(curl -s -X 'GET' 'https://api.environments.bunnyshell.com/api/projects/168/environments?page=1' \
+envid=$(curl -s -X 'GET' 'https://api.environments.bunnyshell.com/api/projects/$PROJECT/environments?page=1' \
   -H 'accept: application/ld+json' \
   -H "X-AUTH-TOKEN: $AUTH_TOKEN" | grep -Po "id.*$GIT_BRANCH" | sed -E 's#.*"id":"?([^,"]*)"?.*#\1#')
 
 [[ -z "$envid" ]] && { echo "envid is empty" ; exit 1; }
   
-unique=$(curl -s -X 'GET' 'https://api.environments.bunnyshell.com/api/projects/168/environments?page=1' \
+unique=$(curl -s -X 'GET' 'https://api.environments.bunnyshell.com/api/projects/$PROJECT/environments?page=1' \
   -H 'accept: application/ld+json' \
   -H "X-AUTH-TOKEN: $AUTH_TOKEN" | grep -Po "id.*$GIT_BRANCH" | sed -E 's/.*"unique":"?([^,"]*)"?.*/\1/')
 
@@ -26,5 +26,6 @@ do
 done
 
 echo "-- performing tests"
+sleep 10
 
 curl "https://api-$unique.bunnyenv.com/" | grep "bunnyshell-neo.*is_amazing" && echo "-- test SUCCESSFUL" || echo "-- test FAILED"
