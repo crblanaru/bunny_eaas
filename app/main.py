b7c892708b7f227aff30804c7f4d354939bbb835
cristianbl <crblanaru@gmail.com>
2022-07-21 10:34:12 +0200

    writing some logs

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from asyncore import write
 from fastapi import FastAPI
 import os
 import random
@@ -10,5 +11,9 @@ def hello_world():
     api_key = os.getenv('API_KEY')
     db_endpoint = os.getenv('DBENDPOINT')
     my_rand = random.randint(100,999)
+    with open('/var/log/dog_breeds.txt', 'w') as writer:
+        writer.write(str(my_rand))
+        writer.write('\n')
+    # Further file processing goes here
     return {"bunnyshell-neo": "is_amazing", "api_key": api_key, "db_endpoints": db_endpoint}
     #return {"bunnyshell-neo": "is_amazing", "api_key": api_key, "random": my_rand}
